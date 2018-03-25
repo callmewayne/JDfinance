8bb7f7279de16e2879558e74fc295743f27689b3
callmewayne <591437639@qq.com>
2018-03-25 20:39:23 +0800

    完成首页

---
+++ app/js/home/borrow.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <Panel title='极速借贷' :class='$style.panel'>
+       <section :class='$style.content'>
+             <router-link :to="{name:'home'}">
+                 <img src='//img12.360buyimg.com/jrpmobile/jfs/t14707/121/2335216734/37656/5611f3d9/5a97704dNca58d124.png?width=750&height=270'>
+             </router-link>
+       </section>
+  </Panel>
+</template>
+<script>
+import Panel from "../core/panel.vue";
+export default {
+  components: {
+    Panel
+  }
+};
+</script>
+<style lang="scss" module>
+@import "../../css/element.scss";
+.panel {
+  @include panel;
+  .content {
+    padding-bottom: 40px;
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+</style>
+
--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,22 +3,40 @@
         <Heador />
         <Slider />
         <Novice />
+        <Borrow />
+        <Money />
+        <Product />
+        <Life />
+        <CampanyInfo />
+        <NavBar />
     </div>
 </template>
 
 <script>
-import Heador from '../public/header.vue'
-import Slider from './hSlider.vue'
-import Novice from './novice.vue'
+import Heador from "../public/header.vue";
+import Slider from "./hSlider.vue";
+import Novice from "./novice.vue";
+import Borrow from "./borrow.vue";
+import Money from "./money.vue";
+import Product from './product.vue';
+import Life from './life.vue';
+import CampanyInfo from '../public/footer.vue';
+import NavBar from '../public/navbar.vue'
 export default {
-  components:{
+  components: {
     Heador,
     Slider,
-    Novice
+    Novice,
+    Borrow,
+    Money,
+    Product,
+    Life,
+    CampanyInfo,
+    NavBar
   }
-}
+};
 </script>
 
 <style lang="scss" module>
- 
+
 </style>
